07815ea18bc073af203e8b5b7dd0ac12e7badd45
Alexis Franco <ronaldofranco196@gmail.com>
2023-10-19 16:09:50 -0300

    filtrado por busqueda

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,9 +1,14 @@
 const cardsContainer = document.getElementById('cards');
+const search = document.getElementById('search-input');
 
 fetch('./data.json')
     .then(res => res.json())
     .then(data => {
-        let cardUSer = data.map(user => {
+        // Mapea los datos para crear tarjetas
+        const users = data.map(user => createCard(user)).join('');
+        cardsContainer.innerHTML = users;
+
+        function createCard(user) {
             return `
             <div class="card-mentoring">
                 <div class="mentor">
@@ -12,10 +17,9 @@ fetch('./data.json')
                         <span>${user.mentor.rol}</span>
                         <h4 class="card-title">${user.mentor.nombre} ${user.mentor.apellido}</h4>
                     </div>
-                    
                 </div>
                 <div class="sala">
-                    <p class="text-sala">Sala: ${user.mentor.sala}
+                    <p class="text-sala">Sala: ${user.mentor.sala}</p>
                 </div>
                 <div class="mentee">
                     <div class='info-mentee'>
@@ -24,8 +28,19 @@ fetch('./data.json')
                     </div>
                     <img class="img-perfil" src="${user.mentee.foto_perfil}" class="card-img-top" alt="${user.mentor.nombre}">
                 </div>
-            </div>`
-        })
-        cardsContainer.innerHTML = cardUSer.join('');
+            </div>`;
+        }
+
+        search.addEventListener('keyup', (e) => {
+            const busqueda = e.target.value.toLowerCase();
+            const filteredUsers = data.filter(user => {
+                const mentorName = `${user.mentor.nombre} ${user.mentor.apellido}`.toLowerCase();
+                const menteeName = `${user.mentee.nombre} ${user.mentee.apellido}`.toLowerCase();
+                return mentorName.includes(busqueda) || menteeName.includes(busqueda);
+            });
+
+            const filteredHTML = filteredUsers.map(user => createCard(user)).join('');
+            cardsContainer.innerHTML = filteredHTML;
+        });
     })
     .catch(err => console.log(err))
