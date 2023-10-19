016d3f50af0ba22b0296216fab91fa8b264166da
Alexis Franco <ronaldofranco196@gmail.com>
2023-10-19 11:53:26 -0300

    JSON sin linkedin

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -8,12 +8,15 @@ fetch('./data.json')
             <div class="card-mentoring">
                 <div>
                     <h2>${user.mentor.rol}</h2>
-                    <img src="${user.mentor.foto_perfil}" class="card-img-top" alt="${user.mentor.nombre}">
+                    <img class="img-perfil" src="${user.mentor.foto_perfil}" class="card-img-top" alt="${user.mentor.nombre}">
                     <h5 class="card-title">${user.mentor.nombre} ${user.mentor.apellido}</h5>
+                </div>
+                <div class="sala">
+                    <p class="card-text">Sala: ${user.mentor.sala}
                 </div>
                 <div class="card-body">
                     <h2>${user.mentee.rol}</h2>
-                    <img src="${user.mentee.foto_perfil}" class="card-img-top" alt="${user.mentor.nombre}">
+                    <img class="img-perfil" src="${user.mentee.foto_perfil}" class="card-img-top" alt="${user.mentor.nombre}">
                     <p class="card-text">${user.mentee.nombre} ${user.mentee.apellido}</p>
                 </div>
             </div>`
